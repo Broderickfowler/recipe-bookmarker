b2e8bf671087c9dafb27729651f6a77b20b0592a
britt <britt.cumens@gmail.com>
2018-05-03 13:02:11 -0400

    edited post signup route to save user to DB and create session

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,19 @@ class UserController < ApplicationController
   end
 
   post '/signup' do
-    binding.pry
+    @user = User.create(params[:user])
+
+    #TODO: Add validation to the model class to ensure NO USERNAME DUPLICATES
+
+    if !!@user.id
+      session[:name] = @user.username
+      session[:id] = @user.id
+      # redirect to user's index page
+      # flash message stating "Successfully created user"
+    else
+      #flash message stating user must fill in all signup information
+      redirect to '/signup'
+    end
+
   end
 end
