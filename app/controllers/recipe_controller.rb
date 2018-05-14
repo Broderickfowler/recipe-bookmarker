2f4761ec5d3f78dd3fb8e4459f1a44b4ce66cfc1
britt <britt.cumens@gmail.com>
2018-05-14 11:10:42 -0400

    added restful routing comments to recipe controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,17 +1,20 @@
 require './config/environment'
 
 class RecipeController < ApplicationController
+  #GET - INDEX
   get '/recipes' do
     @user = Helpers.current_user(session)
     @recipes = Recipe.all
     erb :'recipes/index'
   end
 
+  #GET - NEW
   get '/recipes/new' do
     @creator = Helpers.current_user(session)
     erb :'recipes/new'
   end
 
+  #GET - SHOW
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @creator = User.find_by_id(@recipe.creator_id)
@@ -19,15 +22,19 @@ class RecipeController < ApplicationController
     erb :'recipes/show'
   end
 
+  #GET - EDIT
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :'recipes/edit'
   end
 
+  #POST - CREATE
   post '/recipes' do
     #TODO: Add validations to Recipe model to ensure good data.
     #QUESTION: What is considered good data for that model?
     @recipe = Recipe.create(params[:recipe])
     redirect "/recipes/#{@recipe.id}"
   end
+
+  
 end
--- spec.md
+++ spec.md
@@ -20,9 +20,15 @@ Specs:
   - Buttons and links on Recipe Show pages only reveal "Edit" option if the user.id == creator.id of the recipe
 
 - [ ] Ensure that the belongs_to resource has routes for Creating, Reading, Updating and Destroying
+  
+
+
 - [x] Include user input validations
   - Models ensure a user account must include username and password
+
 - [ ] Display validation failures to user with error message (example form URL e.g. /posts/new)
+
+
 - [ ] Your README.md includes a short description, install instructions, a contributors guide and a link to the license for your code
 
 Confirm
