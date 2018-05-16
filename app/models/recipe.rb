7adc5cb3a8595742ef3cfee3c7b932d74ae7e194
britt <britt.cumens@gmail.com>
2018-05-16 13:54:33 -0400

    added validations to Recipe class

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ActiveRecord::Base
   has_many :user_recipes
   has_many :users, through: :user_recipes
   belongs_to :creator, class_name: "User"
+
+  validates_presence_of :name, :url
+  validates :name, uniqueness: true
 end
